26b2775a196d3a4b7da4e2212f985728ef108a6f
thepaedu <patrick.bettler@icloud.com>
2025-01-09 14:22:15 +0100

    Task 3: Fixed Linter Error (removed or used word2)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { generate } from 'random-words';
 import chalk from 'chalk';
+
 export const sum = (a, b) => {
   return a + b;
 };
 
 const word = generate();
-const word2 = generate();
 console.log(
   `Hello!!! 
   The ${chalk.redBright('random word')} of today is: ${chalk.bgRed(word)}. 
